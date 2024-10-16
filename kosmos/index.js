9860775c5c4df8d95c1dd34845d144be7088227f
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-10-16 16:49:31 -0600

    primer commit

---
+++ kosmos/index.js
@@ -0,0 +1,144 @@
+const tbMontoMinimoMaculino = [
+  { a: { rangoEdad: [0, 26], monto: 100 } },
+  { b: { rangoEdad: [0, 26], monto: 1000 } },
+  { c: { rangoEdad: [0, 26], monto: 400 } },
+  { d: { rangoEdad: [0, 26], monto: 400 } },
+  { a: { rangoEdad: [27, 27], monto: 400 } },
+  { b: { rangoEdad: [27, 27], monto: 600 } },
+  { c: { rangoEdad: [27, 27], monto: 200 } },
+  { d: { rangoEdad: [27, 27], monto: 300 } },
+  { a: { rangoEdad: [28, 28], monto: 900 } },
+  { b: { rangoEdad: [28, 28], monto: 1000 } },
+  { c: { rangoEdad: [28, 28], monto: 200 } },
+  { d: { rangoEdad: [28, 28], monto: 500 } },
+  { a: { rangoEdad: [29, 29], monto: 100 } },
+  { b: { rangoEdad: [29, 29], monto: 1000 } },
+  { c: { rangoEdad: [29, 29], monto: 1000 } },
+  { d: { rangoEdad: [29, 29], monto: 900 } },
+  { a: { rangoEdad: [30, Infinity], monto: 600 } },
+  { b: { rangoEdad: [30, Infinity], monto: 1000 } },
+  { c: { rangoEdad: [30, Infinity], monto: 600 } },
+  { d: { rangoEdad: [30, Infinity], monto: 1000 } }
+]
+
+const tbMontoMinimoFemenino = [
+  { a: { rangoEdad: [0, 24], monto: 800 } },
+  { b: { rangoEdad: [0, 24], monto: 800 } },
+  { c: { rangoEdad: [0, 24], monto: 200 } },
+  { d: { rangoEdad: [0, 24], monto: 500 } },
+  { a: { rangoEdad: [25, 25], monto: 800 } },
+  { b: { rangoEdad: [25, 25], monto: 700 } },
+  { c: { rangoEdad: [25, 25], monto: 900 } },
+  { d: { rangoEdad: [25, 25], monto: 1000 } },
+  { a: { rangoEdad: [26, 26], monto: 800 } },
+  { b: { rangoEdad: [26, 26], monto: 100 } },
+  { c: { rangoEdad: [26, 26], monto: 700 } },
+  { d: { rangoEdad: [26, 26], monto: 600 } },
+  { a: { rangoEdad: [27, 27], monto: 600 } },
+  { b: { rangoEdad: [27, 27], monto: 600 } },
+  { c: { rangoEdad: [27, 27], monto: 800 } },
+  { d: { rangoEdad: [27, 27], monto: 400 } },
+  { a: { rangoEdad: [28, Infinity], monto: 200 } },
+  { b: { rangoEdad: [28, Infinity], monto: 700 } },
+  { c: { rangoEdad: [28, Infinity], monto: 100 } },
+  { d: { rangoEdad: [28, Infinity], monto: 700 } }
+]
+
+const tblMontoMaximoMasculino = [
+  { a: { rangoEdad: [0, 26], monto: 4900 } },
+  { b: { rangoEdad: [0, 26], monto: 4700 } },
+  { c: { rangoEdad: [0, 26], monto: 5000 } },
+  { d: { rangoEdad: [0, 26], monto: 4400 } },
+  { a: { rangoEdad: [27, 27], monto: 4700 } },
+  { b: { rangoEdad: [27, 27], monto: 4400 } },
+  { c: { rangoEdad: [27, 27], monto: 4700 } },
+  { d: { rangoEdad: [27, 27], monto: 4700 } },
+  { a: { rangoEdad: [28, 28], monto: 4600 } },
+  { b: { rangoEdad: [28, 28], monto: 5000 } },
+  { c: { rangoEdad: [28, 28], monto: 5000 } },
+  { d: { rangoEdad: [28, 28], monto: 4300 } },
+  { a: { rangoEdad: [29, 29], monto: 4600 } },
+  { b: { rangoEdad: [29, 29], monto: 4400 } },
+  { c: { rangoEdad: [29, 29], monto: 4200 } },
+  { d: { rangoEdad: [29, 29], monto: 4900 } },
+  { a: { rangoEdad: [30, Infinity], monto: 4500 } },
+  { b: { rangoEdad: [30, Infinity], monto: 4900 } },
+  { c: { rangoEdad: [30, Infinity], monto: 4600 } },
+  { d: { rangoEdad: [30, Infinity], monto: 4300 } }
+]
+
+const tblMontoMaximoFemenino = [
+  { a: { rangoEdad: [0, 24], monto: 4000 } },
+  { b: { rangoEdad: [0, 24], monto: 4700 } },
+  { c: { rangoEdad: [0, 24], monto: 4600 } },
+  { d: { rangoEdad: [0, 24], monto: 5000 } },
+  { a: { rangoEdad: [25, 25], monto: 4200 } },
+  { b: { rangoEdad: [25, 25], monto: 4200 } },
+  { c: { rangoEdad: [25, 25], monto: 4900 } },
+  { d: { rangoEdad: [25, 25], monto: 4900 } },
+  { a: { rangoEdad: [26, 26], monto: 4100 } },
+  { b: { rangoEdad: [26, 26], monto: 4500 } },
+  { c: { rangoEdad: [26, 26], monto: 4600 } },
+  { d: { rangoEdad: [26, 26], monto: 4700 } },
+  { a: { rangoEdad: [27, 27], monto: 4200 } },
+  { b: { rangoEdad: [27, 27], monto: 4300 } },
+  { c: { rangoEdad: [27, 27], monto: 4700 } },
+  { d: { rangoEdad: [27, 27], monto: 5000 } },
+  { a: { rangoEdad: [28, Infinity], monto: 4500 } },
+  { b: { rangoEdad: [28, Infinity], monto: 4400 } },
+  { c: { rangoEdad: [28, Infinity], monto: 4000 } },
+  { d: { rangoEdad: [28, Infinity], monto: 4300 } }
+]
+
+const tblGeneral = {
+  m: {
+    tbMontoMinimo: tbMontoMinimoMaculino,
+    tblMontoMaximo: tblMontoMaximoMasculino
+  },
+  f: {
+    tbMontoMinimo: tbMontoMinimoFemenino,
+    tblMontoMaximo: tblMontoMaximoFemenino
+  }
+}
+
+
+const calculoMotor = (tipoNomina, fechaPrimerEmpleo, genero) => {
+  const [dia, mes, año] = fechaPrimerEmpleo.toISOString().split('T')[0].split('/') // Divide la cadena en partes
+  const fechaSerializada = new Date(`${año}-${mes}-${dia}`);
+  const fechaActual = new Date();
+  const diffEnMeses = (fechaActual.getFullYear() - fechaSerializada.getFullYear()) * 12 + (fechaActual.getMonth() - fechaSerializada.getMonth());
+  // encontrar el monto minimo con el tipo de nomina, la fecga del primer empleo en meses y el genero
+  const montoMinimo = tblGeneral[genero].tbMontoMinimo.find(item => {
+    for (const key in item) {
+      if (item.hasOwnProperty(key)) {
+        const rangoEdad = item[key].rangoEdad;
+        if (diffEnMeses >= rangoEdad[0] && diffEnMeses <= rangoEdad[1] && key.toUpperCase() === tipoNomina.toUpperCase()) {
+          return item;
+        }
+      }
+    }
+  });
+
+  const montoMaximo = tblGeneral[genero].tblMontoMaximo.find(item => {
+    for (const key in item) {
+      if (item.hasOwnProperty(key)) {
+        const rangoEdad = item[key].rangoEdad;
+        if (diffEnMeses >= rangoEdad[0] && diffEnMeses <= rangoEdad[1] && key.toUpperCase() === tipoNomina.toUpperCase()) {
+          return item;
+        }
+      }
+    }
+  });
+
+  const p1 = montoMinimo[tipoNomina].monto + Math.sqrt(montoMaximo[tipoNomina].monto - montoMinimo[tipoNomina].monto);
+  const p2 = montoMinimo[tipoNomina].monto + 0.0175 * (montoMaximo[tipoNomina].monto - montoMinimo[tipoNomina].monto);
+
+  return {
+    montoMinimo: montoMinimo[tipoNomina].monto,
+    montoMaximo: montoMaximo[tipoNomina].monto,
+    p1,
+    p2
+  }
+}
+
+console.log(calculoMotor("a", new Date("2022-12-06"), "f"))
